c7f8cf69e4290bbd75dd24656c0baa4a44386d69
SuhasMarade <suhasmarade99@gmail.com>
2023-08-28 16:36:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Person from "./Person";
+import Button from "./Button";
+import Header from "./Header";
+import List from "./List";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <>
+       <Person name = "Amit" age = "23"/>
+       <Button text = "hit me" onClick = {()=>  console.log("You hit me hard")}/>
+       <Header title = "React Assignment"/>
+       <List items = {['red', 'green', 'blue', 'indigo']}/>
+    </>
+
   );
 }
 
